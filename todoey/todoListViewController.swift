be933a5704cd3f1cf41f74fceb8b0cff05eddb2e
Anmol7869 <anmol.mallick2017@vitstudent.ac.in>
2019-08-05 12:23:16 +0530

    Local data storage

--- todoey/todoListViewController.swift
+++ todoey/todoListViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class todoListViewController : UITableViewController {
     
     
-    let itemArray = ["find mike", "buy eggs", "destroy demimorgon"]
+    var itemArray = ["find mike", "buy eggs", "destroy demimorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            itemArray = items
+        }
+        
+        
     }
 
     
@@ -45,7 +53,34 @@ class todoListViewController : UITableViewController {
         }
     }
     
-
+//MARK - To add new items
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New List Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+        
+        self.itemArray.append(textField.text!)
+            
+        self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+        self.tableView.reloadData()
+        
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+//          print(alertTextField.text)
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
    
     
    
